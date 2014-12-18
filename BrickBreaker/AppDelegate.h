39c049d10e8ca51f01152219df47d7cd8017e6ad
ETHAN PEACOCK <PEACOCKETHAN@GMAIL.COM>
2014-12-18 10:03:33 -0600

    Initial Commit

---
+++ BrickBreaker/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BrickBreaker
+//
+//  Created by Ethan Peacock on 12/18/14.
+//  Copyright (c) 2014 EthanPeacock. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BrickBreaker/GameViewController.h
@@ -0,0 +1,14 @@
+//
+//  GameViewController.h
+//  BrickBreaker
+//
+
+//  Copyright (c) 2014 EthanPeacock. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <SpriteKit/SpriteKit.h>
+
+@interface GameViewController : UIViewController
+
+@end
